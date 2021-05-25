18256431fca17c3231326dbf17afae7b93b95160
JMMirza <janmuhammadmirza@gmail.com>
2021-05-25 11:43:33 +0500

    25/5 task

--- oopConcepts.js
+++ oopConcepts.js
@@ -47,18 +47,163 @@
 // console.log(blueBird('jm', 'whitre', 0))
 
 
-//Understand Own Properties
-function Bird(name) {
-    this.name = name;
-    this.numLegs = 2;
-}
+// //Understand Own Properties
+// function Bird(name) {
+//     this.name = name;
+//     this.numLegs = 2;
+// }
 
-let canary = new Bird("Tweety");
-let ownProps = [];
-// Only change code below this line
-for (let property in canary) {
-    if (canary.hasOwnProperty(property)) {
-        ownProps.push(property)
+// let canary = new Bird("Tweety");
+// let ownProps = [];
+// // Only change code below this line
+// for (let property in canary) {
+//     if (canary.hasOwnProperty(property)) {
+//         ownProps.push(property)
+//     }
+// }
+// console.log(ownProps)
+
+// //difference between own property and prototype property
+
+// function Dog(name) {
+//     this.name = name;
+// }
+
+// Dog.prototype.numLegs = 4;
+
+// let beagle = new Dog("Snoopy");
+
+// let ownProps = [];
+// let prototypeProps = [];
+
+// // Only change code below this line
+// for (let property in beagle) {
+//     if (beagle.hasOwnProperty(property)) {
+//         ownProps.push(property);
+//     } else {
+//         prototypeProps.push(property);
+//     }
+// }
+
+// console.log(ownProps);
+// console.log(prototypeProps);
+
+// //Change the Prototype to a New Object
+// function Dog(name) {
+//     this.name = name;
+// }
+
+// Dog.prototype = {
+//     // Only change code below this line
+//     numLegs: 4,
+//     eat: () => {
+//         console.log("nom nom nom");
+//     },
+//     describe: () => {
+//         console.log("My name is " + this.name);
+//     }
+
+// };
+
+// //inheritance and adding methods
+// function Animal() {}
+// Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+// function Dog() {}
+
+// Dog.prototype = Object.create(Animal.prototype)
+// Dog.prototype.constructor = Dog;
+// Dog.prototype.bark = () => {
+//     console.log("Woof!")
+// }
+
+// let beagle = new Dog();
+// beagle.bark()
+// beagle.eat()
+
+// //override a function
+
+// function Bird() {}
+
+// Bird.prototype.fly = function() { return "I am flying!"; };
+
+// function Penguin() {}
+// Penguin.prototype = Object.create(Bird.prototype);
+// Penguin.prototype.constructor = Penguin;
+
+// // Only change code below this line
+// Penguin.prototype.fly = function() {
+//     return "Alas, this is a flightless bird."
+// }
+
+
+// // Only change code above this line
+
+// let penguin = new Penguin();
+// console.log(penguin.fly());
+
+// //mixin property add common behaviour between unrelated objects
+// let bird = {
+//     name: "Donald",
+//     numLegs: 2
+// };
+
+// let boat = {
+//     name: "Warrior",
+//     type: "race-boat"
+// };
+
+// // Only change code below this line
+
+// let glideMixin = (object) => {
+//     object.glide = () => {
+//         console.log("gliding!")
+//     }
+// }
+// glideMixin(bird)
+// glideMixin(boat)
+// bird.glide()
+// boat.glide()
+
+//use IIFE to create a module
+
+/* 
+CONVORTED BELOW TWO FUNCTIONS INTO A MODULE 
+
+let isCuteMixin = function(obj) {
+  obj.isCute = function() {
+    return true;
+  };
+};
+let singMixin = function(obj) {
+  obj.sing = function() {
+    console.log("Singing to an awesome tune");
+  };
+}; */
+let isCuteMixin = function(obj) {
+    obj.isCute = function() {
+        return true;
+    };
+};
+let singMixin = function(obj) {
+    obj.sing = function() {
+        console.log("Singing to an awesome tune");
+    };
+};
+
+let funModule = (
+    function() {
+        return {
+            isCuteMixin: function(obj) {
+                obj.isCute = function() {
+                    return true;
+                };
+            },
+            singMixin: function(obj) {
+                obj.sing = function() {
+                    console.log("Singing to an awesome tune");
+                };
+            }
+        }
     }
-}
-console.log(ownProps)
+)()
