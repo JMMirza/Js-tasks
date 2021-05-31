655ded7f8af87303f3e5fe4d4511c8e9731019c1
jmmirza <janmuhammadmirza@gmail.com>
2021-05-31 18:03:49 +0500

    31/05

---
+++ abc.js
@@ -0,0 +1,27 @@
+// // const { countdown, lookUpProfile } = require('./basicsOfJS');
+// array = [{ id: 1, name: "ali", friends: ['jibran', 'usman', 'ali'] }, { id: 2, name: "kamal", friends: ['jamshed', 'jibran', 'usman'] }];
+
+// function commonFriends(arr) {
+//     let final = []
+//     let user = arr
+//         .map(user => user['friends'])
+//     let array = user.filter(usedr => {
+//         if (user.includes(usedr, 0)) {
+//             final.push(usedr)
+//         }
+//     })
+//     console.log(final);
+// }
+
+// function allFriends(arr) {
+//     let array = arr.map(item => item.friends)
+//     console.log("List of all friends", array)
+
+// }
+// allFriends(array)
+// commonFriends(array)
+let user = {
+    id: 1
+}
+user['name2'] = "jm"
+console.log(user)
--- basicsOfJS.js
+++ basicsOfJS.js
@@ -280,44 +280,44 @@
 // ]);
 
 
-// var contacts = [{
-//         "firstName": "Akira",
-//         "lastName": "Laine",
-//         "number": "0543236543",
-//         "likes": ["Pizza", "Coding", "Brownie Points"]
-//     },
-//     {
-//         "firstName": "Harry",
-//         "lastName": "Potter",
-//         "number": "0994372684",
-//         "likes": ["Hogwarts", "Magic", "Hagrid"]
-//     },
-//     {
-//         "firstName": "Sherlock",
-//         "lastName": "Holmes",
-//         "number": "0487345643",
-//         "likes": ["Intriguing Cases", "Violin"]
-//     },
-//     {
-//         "firstName": "Kristian",
-//         "lastName": "Vos",
-//         "number": "unknown",
-//         "likes": ["JavaScript", "Gaming", "Foxes"]
-//     }
-// ];
-//
-// function lookUpProfile(name, prop) {
-//     for (let x = 0; x < contacts.length; x++) {
-//         if (contacts[x].firstName === name) {
-//             if (contacts[x].hasOwnProperty(prop)) {
-//                 console.log(contacts[x][prop]);
-//             } else {
-//                 console.log("No such property");
-//             }
-//         }
-//     }
-//     console.log("No such contact")
-// }
+var contacts = [{
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+function lookUpProfile(name, prop) {
+    for (let x = 0; x < contacts.length; x++) {
+        if (contacts[x].firstName === name) {
+            if (contacts[x].hasOwnProperty(prop)) {
+                console.log(contacts[x][prop]);
+            } else {
+                console.log("No such property");
+            }
+        }
+    }
+    console.log("No such contact")
+}
 
 // // above this line
 
@@ -348,4 +348,5 @@ function countdown(n) {
         return countArray;
     }
 }
-console.log(countdown(5))
+module.exports = { countdown, lookUpProfile }
+    // console.log(countdown(5))
